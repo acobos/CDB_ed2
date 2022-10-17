8f1576885eb3048accf442fa979dc5fe816efa61
Albert Cobos <acobos@ub.edu>
2022-10-17 11:45:01 +0200

    add diagnostic() from ThresholdROC

--- 4_2_categ_paired_exe_solution.R
+++ 4_2_categ_paired_exe_solution.R
@@ -1,22 +1,27 @@
+library(tidyverse)
+library(mosaic)
+
 # reading the data
 d <- rio::import("data/bta.xls")
 head(d)
-
-library(mosaic)
 
 # BTA and reference standard (cystoscopy)
 tally(bta ~ cystoscopy, data=d)
 tally(bta ~ cystoscopy, data=d, format="percent")
 
+# using function diagnostic() of package ThresholdROC
+library(ThresholdROC)
+?diagnostic
+
+tally(fct_rev(bta) ~ fct_rev(cystoscopy), data=d) %>% diagnostic()
 
 # Cytology and reference standard (cystoscopy)
 tally(cytology ~ cystoscopy, data=d)
 x <- tally(cytology ~ cystoscopy, data=d, format="percent")
 x
 
-# get just the requested values
-x[2,2]  # sensitivity
-x[1,1]  # specificity
+tally(fct_rev(cytology) ~ fct_rev(cystoscopy), data=d) %>% diagnostic()
+
 
 # compare sensitivities
 x <- tally(bta ~ cytology, data=d, subset = cystoscopy == "Positive")
